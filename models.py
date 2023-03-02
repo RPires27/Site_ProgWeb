db7ff11d893da7002bc8cb9b56ed8bae8861c895
Rodrigo Pires <pirex02@gmail.com>
2023-03-02 17:48:55 +0000

    novas tabelas

--- models.py
+++ models.py
@@ -13,6 +13,7 @@ class User(db.Model):
     phone = db.Column(db.String(80), nullable=False)
     created = db.Column(db.Date, default=datetime.utcnow)
     role_id = db.Column(db.String(80), db.ForeignKey("role.role"))
+    role = db.relationship("Role")
      
 
     
@@ -32,6 +33,62 @@ class Morada(db.Model):
     add_line2 = db.Column(db.String(80), nullable=False)
     city = db.Column(db.String(80), nullable=False)
     postal_code = db.Column(db.String(80), nullable=False)
+    created = db.Column(db.Date, default=datetime.utcnow)
+    modified = db.Column(db.Date, default=datetime.utcnow)
+
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User')
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    created = db.Column(db.Date, default=datetime.utcnow)
+    modified = db.Column(db.Date, default=datetime.utcnow)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    created = db.Column(db.Date, default=datetime.utcnow)
+    modified = db.Column(db.Date, default=datetime.utcnow)
+    inventory = db.Column(db.Integer, nullable=False)
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(80), nullable=False)
+    created = db.Column(db.Date, default=datetime.utcnow)
+    modified = db.Column(db.Date, default=datetime.utcnow)
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    product = db.relationship('Product')
+    image = db.Column(db.String(80), nullable=False)
+    created = db.Column(db.Date, default=datetime.utcnow)
+    modified = db.Column(db.Date, default=datetime.utcnow)
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User')
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    product = db.relationship('Product')
+    payment_gateway_id = db.Column(db.Integer, db.ForeignKey('payment_gateway.id'), nullable=False)
+    created = db.Column(db.Date, default=datetime.utcnow)
+    modified = db.Column(db.Date, default=datetime.utcnow)
+
+
+class PaymentGateway(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(80), nullable=False)
     created = db.Column(db.Date, default=datetime.utcnow)
     modified = db.Column(db.Date, default=datetime.utcnow)
     
